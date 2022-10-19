cebf9da6400811845f616d15a7891dc7d45bc1a4
jesper-sk <jesper.kuiper99@gmail.com>
2022-10-19 12:41:30 +0200

    remove data json

--- metrics.py
+++ metrics.py
@@ -10,5 +10,19 @@ G = graph()
 # connected components and, size of the connected
 # components.
 
+import networkx as nx
+
+order = G.order()
+edges = G.number_of_edges()
 in_deg = G.in_degree()
 out_deg = G.out_degree()
+in_deg_centr = nx.in_degree_centrality(G)
+out_deg_centr = nx.out_degree_centrality(G)
+clustering = nx.clustering(G)
+# diameter = infinite (not strongly connected)
+density = nx.density(G)
+#connnected_components = not implemented
+
+#%% weakly connected components
+big_weak_cc = max(nx.weakly_connected_components(G), key=len)
+
