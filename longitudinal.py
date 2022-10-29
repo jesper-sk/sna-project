ac1e199182f6e51cc633ea9f7f05b389f6d7b577
jesper-sk <47450353+JesperK123456@users.noreply.github.com>
2022-10-29 13:55:03 +0200

    Add framework for scraper

--- longitudinal.py
+++ longitudinal.py
@@ -1,17 +1,16 @@
 #%% Imports
+import json
+
 from hep_ph import *
-from tqdm import tqdm
 import os, time
 from pathlib import Path
+import ijson
+from tqdm import tqdm
 
 PUB_DATA_FILE = './data/pub.csv'
 
+#%%
 
-#%% Variables
-g = citation_graph()
-
-
-#%% Arxiv
 def scrape_arxiv_data(graph, wait: int = 3, start_at: int = 0, write_thresh: int = 15, keep_trying: bool = False):
     waits = [0, 1, 5, 10, 30]
     path = Path(PUB_DATA_FILE)
@@ -23,7 +22,7 @@ def scrape_arxiv_data(graph, wait: int = 3, start_at: int = 0, write_thresh: int
     for i, node in enumerate(graph.nodes):
         if i < start_at:
             continue
-        print(i, node)
+        print(f"{i:6d} {node}")
         success = False
         trial = 0
         while keep_trying or trial < len(waits):
@@ -38,6 +37,8 @@ def scrape_arxiv_data(graph, wait: int = 3, start_at: int = 0, write_thresh: int
                 lines.append(
                     f"{i}|::|{node}|::|{r.title}|::|{';'.join(map(str, r.authors))}|::|{r.published.isoformat()}|::|{r.entry_id}\n")
                 success = True
+            except KeyboardInterrupt:
+                raise
             except Exception as e:
                 print(e)
                 trial += 1
@@ -49,3 +50,21 @@ def scrape_arxiv_data(graph, wait: int = 3, start_at: int = 0, write_thresh: int
         if i % write_thresh == 0:
             with path.open('a') as file:
                 file.writelines(lines)
+
+        time.sleep(wait)
+
+
+if __name__ == '__main__':
+    scrape_arxiv_data(citation_graph(), 2, 676)
+
+# #%%
+#
+# all = []
+# with open('./data/arxiv-metadata-oai-snapshot.json', 'rb') as file:
+#     for line in tqdm(file.readlines()):
+#         all.append(json.loads(line))
+#
+# #%%
+# relevant = []
+# for entry in all:
+#     id = all
